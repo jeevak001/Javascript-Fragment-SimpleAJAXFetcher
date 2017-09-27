ab329ebe4e8a40e5920ed19e9c2749e38021ba84
jeevak001 <jeeva.nasa01@gmail.com>
2017-09-27 23:55:52 +0530

    ok

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8"/>
+	<meta name="viewport" content="initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,width=device-width"/>
+	<title> Main Title </title>
+	<link rel="stylesheet" href="styles.css" />
+	<script src="jquery.js"></script>
+	<script src="script.js"></script>
+</head>
+
+<body>
+	
+	
+	
+</body>
+</html>
